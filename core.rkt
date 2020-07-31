927cd1d5d78a8f53235274062aba307de9dcfa64
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-31 12:46:08 +0800

    simple unification

--- core.rkt
+++ core.rkt
@@ -1,88 +1,69 @@
-#lang typed/racket
-
-(all-defined-out)
+#lang racket
 
 (module+ test
-  (require typed/rackunit))
+  (require rackunit))
 
-(struct Stmt () #:transparent)
-; (= x 1) or (= 1 x) or (= 1 1)
-(struct Stmt= Stmt
-  ([e : Exp]
-   [e2 : Exp])
-  #:transparent)
-; (: x integer)
-(struct Stmt: Stmt
-  ([e : Exp]
-   [t : Typ])
-  #:transparent)
+;;; Environment
+(define (lookup env v)
+  (hash-ref env v))
+(define (extend env v e)
+  (hash-set! env v e))
 
-(struct Typ () #:transparent)
-(struct Exp () #:transparent)
-(struct Val Exp () #:transparent)
-(struct Val:int Val
-  ([v : Integer])
-  #:transparent)
-(struct Val:free Val () #:transparent)
-(struct Exp:var Exp
-  ([name : String])
-  #:transparent)
-(struct Exp:+ Exp
-  ([left : Exp]
-   [right : Exp])
-  #:transparent)
+(define (occurs v t)
+  (match t
+    [`(,t* ...)
+     (ormap (λ (t) (occurs v t)) t*)]
+    (t (equal? v t))))
 
-(define (new-var? [ctx : (Mutable-HashTable String Val)]
-                  [name : String]
-                  [v : Val])
-  : Void
-  (if (hash-has-key? ctx name)
-      (void)
-      (hash-set! ctx name v)))
-(define (lookup/val [ctx : (Mutable-HashTable String Val)]
-                    [name : String])
-  : Val
-  (hash-ref ctx name))
-(define (Val-equal [v1 : Val]
-                   [v2 : Val])
-  : Boolean
-  (match* (v1 v2)
-    [((Val:int i) (Val:int j))
-     (= i j)]
-    [(_ _) #f]))
+(define (unify t1 t2)
+  (match* (t1 t2)
+    [(_ t2) #:when (parameter? t2)
+            (if (or (eqv? t1 (t2)) (not (occurs (t2) t1)))
+                (t2 t1)
+                (error (format "~a occurs in ~a" (t2) t1)))]
+    [(t1 _) #:when (parameter? t1)
+            (unify t2 t1)]
+    [(`(,a* ...) `(,b* ...))
+     (for-each unify a* b*)]
+    [(_ _)
+     (unless (eqv? t1 t2)
+       (error (format "cannot unify ~a and ~a" t1 t2)))]))
 
-(define (unify [ctx : (Mutable-HashTable String Val)]
-               [e1 : Exp]
-               [e2 : Exp])
-  : Boolean
-  (match* (e1 e2)
-    [((Exp:var name) e)
-     (new-var? ctx name (Val:free))
-     (Val-equal (eval ctx e1) (eval ctx e2))]
-    [(e (Exp:var name))
-     (new-var? ctx name (Val:free))
-     (Val-equal (eval ctx e1) (eval ctx e2))]
-    [(e1 e2) (Val-equal (eval ctx e1) (eval ctx e2))]))
-(define (eval [ctx : (Mutable-HashTable String Val)]
-              [e : Exp])
-  : Val
-  (match e
-    [(Exp:+ l r) (Val:int (+ (eval-int ctx l) (eval-int ctx r)))]
-    [(Exp:var name) (lookup/val ctx name)]
-    [(Val:int i) (Val:int i)]))
-(define (eval-int [ctx : (Mutable-HashTable String Val)]
-                  [e : Exp])
-  : Integer
-  (match (eval ctx e)
-    [(Val:int i) i]
-    [_ (error 'type-mismatched "expected integer, but got: ~a" e)]))
+(define (eval tm env)
+  (match tm
+    [`(= ,a ,b)
+     (unify (eval a env) (eval b env))]
+    [`(,a* ...)
+     (map (λ (a) (eval a env)) a*)]
+    [x (cond
+         [(symbol? x)
+          (let* ([v (if (hash-has-key? env x)
+                        (lookup env x)
+                        (make-parameter x))])
+            (extend env x v)
+            v)]
+         [else x])]))
+
+(define (run tm* [env (make-hash)])
+  (for-each (λ (tm) (eval tm env))
+            tm*))
 
 (module+ test
   (test-case
    "unify x 1"
-   (define ctx : (Mutable-HashTable String Val) (make-hash '()))
-   (unify ctx (Val:int 1) (Exp:var "x"))
-   (check-equal? (lookup/val ctx "x")
-                 (Val:int 1)))
-
-  )
+   (define env (make-hash))
+   (run '((= x 1))
+        env)
+   (check-equal? ((lookup env 'x)) 1))
+  (test-case
+   "unify 1 x"
+   (define env (make-hash))
+   (run '((= 1 x))
+        env)
+   (check-equal? ((lookup env 'x)) 1))
+  (test-case
+   "unify (add1 1) (add1 x)"
+   (define env (make-hash))
+   (run '((= (add1 1) (add1 x)))
+        env)
+   (check-equal? ((lookup env 'x)) 1)))
