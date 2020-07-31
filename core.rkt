e86dab97502b8039bcb4e658a1502b91f150bcb0
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-31 14:25:33 +0800

    + z n

--- core.rkt
+++ core.rkt
@@ -15,32 +15,44 @@
      (ormap (λ (t) (occurs v t)) t*)]
     (t (equal? v t))))
 
+(define (extract-p? p)
+  (if (parameter? p)
+      (p)
+      p))
+
 ;;; return #t for success
 (define (unify t1 t2)
   (match* (t1 t2)
     [(_ t2) #:when (and (parameter? t2)
                         (symbol? (t2)))
             (if (or (eqv? t1 (t2)) (not (occurs (t2) t1)))
-                (begin (t2 t1)
+                (begin (t2 (extract-p? t1))
                        #t)
-                (error (format "~a occurs in ~a" (t2) t1)))]
+                (error (format "~a occurs in ~a" (t2) (extract-p? t1))))]
     [(t1 _) #:when (parameter? t1)
             (unify t2 t1)]
+    [(a `(or ,p* ...))
+     (ormap (λ (p)
+              (unify a p)
+              #t)
+            p*)]
+    [(`(or ,p* ...) _)
+     (unify t2 t1)]
     [(`(,a* ...) `(,b* ...))
      (andmap unify a* b*)]
     [(_ _)
-     (let ([t2 (if (parameter? t2) (t2) t2)])
-       (unless (eqv? t1 t2)
-         (error (format "cannot unify ~a and ~a" t1 t2)))
-       #t)]))
+     (unless (eqv? t1 (extract-p? t2))
+       (error (format "cannot unify ~a and ~a" t1 (extract-p? t2))))]))
 
 (define (eval tm env)
   (match tm
     [`(= ,a ,b)
      (unify (eval a env) (eval b env))]
+    [`(quote ,x) x]
     [`(,a* ...)
      (map (λ (a) (eval a env)) a*)]
     [x (cond
+         [(member x '(or + -)) x]
          [(symbol? x)
           (let* ([v (if (hash-has-key? env x)
                         (lookup env x)
@@ -49,36 +61,46 @@
             v)]
          [else x])]))
 
+(define (pretty tm)
+  (match tm
+    [`(,x* ...)
+     (map pretty x*)]
+    [x (extract-p? x)]))
 (define (run tm* [env (make-hash)])
   (for-each (λ (tm)
-              (let* ([e (eval tm env)]
-                    [v (if (parameter? e)
-                           (e)
-                           e)])
-                (displayln v)))
+              (displayln (pretty (eval tm env))))
             tm*))
 
 (run
- '((= (foo 0 1) (foo x y))
-   x
-   y))
+ '((= ('s ('s 'z)) ('s x))
+   x))
+(run
+ '((= (+ 'z n n)
+      (+ 'z 'z r))
+   r))
+(run
+ '((= (or (+ 'z n n)
+          (+ ('s m) n
+             ('s (+ m n))))
+      (+ 'z 'z r))
+   r))
 
 (module+ test
   (test-case
-   "unify x 1"
+   "unify x z"
    (define env (make-hash))
-   (run '((= x 1))
+   (run '((= x 'z))
         env)
-   (check-equal? ((lookup env 'x)) 1))
+   (check-equal? ((lookup env 'x)) 'z))
   (test-case
-   "unify 1 x"
+   "unify z x"
    (define env (make-hash))
-   (run '((= 1 x))
+   (run '((= 'z x))
         env)
-   (check-equal? ((lookup env 'x)) 1))
+   (check-equal? ((lookup env 'x)) 'z))
   (test-case
-   "unify (add1 1) (add1 x)"
+   "unify (s z) (s x)"
    (define env (make-hash))
-   (run '((= (add1 1) (add1 x)))
+   (run '((= '(s z) ('s x)))
         env)
-   (check-equal? ((lookup env 'x)) 1)))
+   (check-equal? ((lookup env 'x)) 'z)))
